d42289590dfcdf950fa4bfeb8390193a71ecf17a
Mohamed Reda Naja <mohamed-reda.naja@ensimag.grenoble-inp.fr>
2016-02-08 12:09:03 +0100

    merge

--- TP1_NAJAM_AMIRAC/src/main02.cpp
+++ TP1_NAJAM_AMIRAC/src/main02.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "Dvector.h"
 
-using namespace std;
-
 int main()
 {
     //Dvector vect(3,2);
     //vect.display(cout);
-    Dvector(3, 2).display(cout);
+    Dvector(3, 2).display(std::cout);
+
     return 0;
 }
