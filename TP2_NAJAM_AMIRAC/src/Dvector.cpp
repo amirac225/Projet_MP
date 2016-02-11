289943c7cef412f43d394d163028f6f92308bf1d
Mohamed Reda Naja <mohamed-reda.naja@ensimag.grenoble-inp.fr>
2016-02-11 11:40:00 +0100

    modif

--- TP2_NAJAM_AMIRAC/src/Dvector.cpp
+++ TP2_NAJAM_AMIRAC/src/Dvector.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 
 using namespace std;
+/* Les Constructeurs : */
 
 /* Constructeur par défaut surchargé */
 Dvector::Dvector() : m_double(0), m_taille(0)
@@ -13,7 +14,7 @@ Dvector::Dvector() : m_double(0), m_taille(0)
 }
 
 /* Initialisation d'un vecteur à partir d'un fichier se trouvant à str */
-Dvector::Dvector(string str) : m_double(0), m_taille(0)
+Dvector::Dvector(string const& str) : m_double(0), m_taille(0)
 {
     cout << "Appel du constructeur faisant appel au fichier " << str << endl;
     /* Ouverture du fichier */
@@ -66,8 +67,10 @@ Dvector::Dvector(Dvector const& dvector) : m_double(0), m_taille(dvector.m_taill
     }
 }
 
+/* La surcharges d'opérateurs : */
+
 /* Surcharge de l'operateur d'affectation puisqu'on a surchargé un constructeur de recopie */
- Dvector& Dvector::operator=(Dvector const& dvector)
+Dvector& Dvector::operator=(Dvector const& dvector)
 {
     if(this != &dvector)
     {
@@ -81,6 +84,157 @@ Dvector::Dvector(Dvector const& dvector) : m_double(0), m_taille(dvector.m_taill
     }
     return *this;
 }
+
+/* Operateur d'accession : */
+double& Dvector::operator[](int const& i)
+{
+    return m_double[i];
+}
+
+/* Operateur de comparaison : */
+bool operator==(Dvector const& dvector1, Dvector const& dvector2)
+{
+    for(int i = 0; i < dvector1.size(); i++)
+    {
+        if(dvector1.m_double[i] != dvector2.m_double[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool operator!=(Dvector const& dvector1, Dvector const& dvector2)
+{
+    return !(dvector1 == dvector2);
+}
+
+// Doivent avoir la meme taille :
+Dvector& Dvector::operator+=(Dvector const& dvector)
+{
+    for(int i = 0; i < m_taille; i++)
+    {
+        m_double[i] += dvector.m_double[i];
+    }
+    return *this;
+}
+
+/* Operateur d'ajout : */
+Dvector operator+(Dvector const& dvector1, Dvector const& dvector2)
+{
+    Dvector tmpvector(dvector1);
+    tmpvector += dvector2;
+    return tmpvector;
+}
+
+Dvector operator-(Dvector const& dvector)
+{
+    return Dvector(dvector.size(),0.)-dvector;
+}
+
+// doivent avoir la meme taille :
+Dvector& Dvector::operator-=(Dvector const& dvector)
+{
+    for(int i = 0; i < m_taille; i++)
+    {
+        m_double[i] -= dvector.m_double[i];
+    }
+    return *this;
+}
+
+/* Operateur d'ajout : */
+Dvector operator-(Dvector const& dvector1, Dvector const& dvector2)
+{
+    Dvector tmpvector(dvector1);
+    tmpvector -= dvector2;
+    return tmpvector;
+}
+
+Dvector& Dvector::operator+=(double const& dvector)
+{
+    for(int i = 0; i < m_taille; i++)
+    {
+        m_double[i] += dvector;
+    }
+    return *this;
+}
+
+Dvector operator+(double const& dvector1, Dvector const& dvector2)
+{
+    Dvector dvector(dvector2);
+    dvector += dvector1;
+    return dvector;
+}
+
+Dvector operator+(Dvector const& dvector2, double const& dvector1)
+{
+    return dvector1 + dvector2;
+}
+
+Dvector& Dvector::operator-=(double const& dvector)
+{
+    for(int i = 0; i < m_taille; i++)
+    {
+        m_double[i] -= dvector;
+    }
+    return *this;
+}
+
+Dvector operator-(double const& dvector1, Dvector const& dvector2)
+{
+    Dvector dvector(dvector2.size(),dvector1);
+    dvector -= dvector2;
+    return dvector;
+}
+
+Dvector operator-(Dvector const& dvector2, double const& dvector1)
+{
+    return dvector1 - dvector2;
+}
+
+Dvector& Dvector::operator*=(double const& dvector)
+{
+    for(int i = 0; i < m_taille; i++)
+    {
+        m_double[i] *= dvector;
+    }
+    return *this;
+}
+
+Dvector operator*(double const& dvector1, Dvector const& dvector2)
+{
+    Dvector dvector(dvector2);
+    dvector *= dvector1;
+    return dvector;
+}
+
+Dvector operator*(Dvector const& dvector2, double const& dvector1)
+{
+    return dvector1 * dvector2;
+}
+
+Dvector& Dvector::operator/=(double const& dvector)
+{
+    for(int i = 0; i < m_taille; i++)
+    {
+        m_double[i] /= dvector;
+    }
+    return *this;
+}
+
+Dvector operator/(Dvector const& dvector2, double const& dvector1)
+{
+    Dvector dvector(dvector2);
+    dvector /= dvector1;
+    return dvector;
+}
+
+ostream& operator<<(ostream &flux, Dvector const& dvector)
+{
+    return flux;
+}
+
+/* Les methodes : */
 
 /* Affichage du vecteur */
 void Dvector::display(std::ostream& str) const
@@ -105,6 +259,32 @@ void Dvector::fillRandomly()
     {
         m_double[i] = rand()/(double)RAND_MAX;
     }
+}
+
+void Dvector::resize(int const& taille, ...)
+{
+    double *z_double(0);
+    va_list ap;
+    va_start(ap, taille);
+    z_double = new double[taille];
+
+    if(taille > m_taille)
+    {
+        memcpy(z_double,m_double,m_taille*sizeof(double));
+        for(int i = m_taille; i < taille; i++)
+        {
+            z_double[i] = va_arg(ap,double);
+        }
+    }
+    else
+    {
+        memcpy(z_double,m_double,taille*sizeof(double));
+    }
+    va_end(ap);
+    
+    delete [] m_double;
+    m_double = z_double;
+    m_taille = taille;
 }
 
 /* Destructeur */
--- TP2_NAJAM_AMIRAC/src/Dvector.h
+++ TP2_NAJAM_AMIRAC/src/Dvector.h
@@ -1,9 +1,11 @@
 #ifndef DVECTOR_H
 #define DVECTOR_H
 
-#include <iomanip>
 #include <iostream>
 #include <string>
+#include <ostream>
+#include <cstring>
+#include <stdarg.h>
 
 /** @brief Classe Dvector modelisant un vecteur réel de taille m_taille.
     @author NAJA Mohamed Reda
@@ -24,18 +26,31 @@ class Dvector
         Dvector(Dvector const& dvector);
         /** Constructeur qui construit un vecteur à partir du fichier str.
         */
-        Dvector(std::string str);
+        Dvector(std::string const& str);
         Dvector& operator=(Dvector const& dvector);
+        Dvector& operator+=(double const& dvector);
+        Dvector& operator-=(double const& dvector);
+        Dvector& operator*=(double const& dvector);
+        Dvector& operator/=(double const& dvector);
+        Dvector& operator+=(Dvector const& dvector);
+        Dvector& operator-=(Dvector const& dvector);
+        double& operator[](int const& i);
+        /** Methode qui modifie la taille du vecteur en question 
+        */
+        void resize(int const& taille, ...);
         /** @return La taille du vecteur.
         */
         int size() const;
         /** Methode qui construit le vecteur à partir de la loi normal sur [0,1]
         */
         void fillRandomly();
+       
         /** Affiche le vecteur.
             @param str : un flux.
         */
         void display(std::ostream& str) const;
+        friend std::ostream& operator<<(std::ostream &flux, Dvector const& dvector);
+        friend bool operator==(Dvector const& dvector1, Dvector const& dvector2);
         /** Destructeur.
         */
         virtual ~Dvector();
@@ -48,6 +63,18 @@ class Dvector
             Taille du vecteur.
         */
         int m_taille;
+
 };
 
+Dvector operator+(double const& dvector1, Dvector const& dvector2);
+Dvector operator+(Dvector const& dvector2, double const& dvector1);
+Dvector operator-(double const& dvector1, Dvector const& dvector2);
+Dvector operator-(Dvector const& dvector2, double const& dvector1);
+Dvector operator*(double const& dvector1, Dvector const& dvector2);
+Dvector operator*(Dvector const& dvector2, double const& dvector1);
+Dvector operator/(Dvector const& dvector2, double const& dvector1);
+Dvector operator+(Dvector const& dvector1, Dvector const& dvector2);
+Dvector operator-(Dvector const& dvector1, Dvector const& dvector2);
+Dvector operator-(Dvector const& dvector);
+bool operator!=(Dvector const& dvector1, Dvector const& dvector2);
 #endif // DVECTOR_H
